ff917bc3868626128427f33b422c20e74465aecd
Hayato Igarshi <igahaya5@gmail.com>
2022-05-19 22:45:20 +0000

    "add Material UI"

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App:React.FC<{}> = () => {
+  return (
+    <div>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+
+export default App;
