b9f892e26c37c3f18b21995f1d389e12535f4e8d
Igarashi <hayato_igarashi@nbf-web.com>
2022-05-29 22:09:24 +0000

    "edit sellAddCards style"

--- src/sellerComponents/sellAdd/sellAddCards.tsx
+++ src/sellerComponents/sellAdd/sellAddCards.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Paper, TextField } from "@mui/material"
+import { Button, Container, Paper, Stack, TextField } from "@mui/material"
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 
 //画像アップロードのinput要素を非表示にする
@@ -9,55 +9,60 @@ let upload = {
 export const SellAddCards = () => {
   return(
     <div>
-      <label htmlFor="image">
-        <Paper elevation={3}
-          sx={{
-          }}
-        >
-          <AddPhotoAlternateOutlinedIcon fontSize="large"/>
-        </Paper>
-      </label>
-      <input
-          style={upload}
-          accept="image/*"
-          type="file"
-          id='image'
-      />
-      <TextField
-        id="outlined-password-input"
-        label="Title"
-        autoComplete="current-password"
-        fullWidth
-      />
-      <TextField
-        id="outlined-password-input"
-        label="Price"
-        autoComplete="current-password"
-        fullWidth
-      />
-      <TextField
-        id="outlined-password-input"
-        label="Place"
-        autoComplete="current-password"
-        fullWidth
-      />
-      <TextField
-        id="outlined-password-input"
-        label="date"
-        autoComplete="current-password"
-        fullWidth
-      />
-      <TextField
-        id="outlined-password-input"
-        label="description"
-        autoComplete="current-password"
-        maxRows={8}
-        fullWidth
-        multiline
-      />
-      <Button>
-        出品
-      </Button>
+      <Container maxWidth="sm" sx= {{pt: 5 }}>
+        <Stack spacing={1}>
+          <label htmlFor="image" >
+            <Paper elevation={3}
+              sx={{
+                height:'30vh',
+              }}
+            >
+              <AddPhotoAlternateOutlinedIcon fontSize="large"/>
+            </Paper>
+          </label>
+          <input
+              style={upload}
+              accept="image/*"
+              type="file"
+              id='image'
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Title"
+            autoComplete="current-password"
+            fullWidth
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Price"
+            autoComplete="current-password"
+            fullWidth
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Place"
+            autoComplete="current-password"
+            fullWidth
+          />
+          <TextField
+            id="outlined-password-input"
+            label="date"
+            autoComplete="current-password"
+            fullWidth
+          />
+          <TextField
+            id="outlined-password-input"
+            label="description"
+            autoComplete="current-password"
+            maxRows={8}
+            fullWidth
+            multiline
+          />
+          <Button variant="outlined">
+            出品
+          </Button>
+        </Stack>
+      </Container>
     </div>
   )
 }
