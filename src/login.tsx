958f7f68a8b44922f83d62f6d91cacc92dbc69a0
Igarashi <hayato_igarashi@nbf-web.com>
2022-05-30 07:07:41 +0000

    "edit login componnts"

--- src/login.tsx
+++ src/login.tsx
@@ -9,14 +9,14 @@ export const Login = ( ) => {
     return(
         <div
 				>
-							<Container maxWidth="sm" sx= {{pt: 5 }}>
-									<Stack spacing={3}>
-										<div>ログインパスワードをSMSで送ります<br />
-										電話番号を入力してください</div>
-										<TextField required label="電話番号" />
-										<Button variant="outlined">確認コードを送信</Button>
-									</Stack>
-								</Container>
+					<Container maxWidth="sm" sx= {{pt: 5 }}>
+							<Stack spacing={3}>
+								<div>ログインパスワードをSMSで送ります<br />
+								電話番号を入力してください</div>
+								<TextField required label="電話番号" />
+								<Button variant="outlined">確認コードを送信</Button>
+							</Stack>
+						</Container>
         </div>
     )
 }
